ca3daddd368b3c7e66c0999b47c134feac1498c6
Gogul11 <gogul0811@gmail.com>
2026-04-08 14:41:25 +0530

    hi

--- App/lib/api.ts
+++ App/lib/api.ts
@@ -3,7 +3,7 @@ import { Platform } from "react-native";
 
 const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_BASE_URL ??
-  (Platform.OS === "android" ? "http://10.16.34.176:5000" : "http://10.16.34.176:5000");
+  (Platform.OS === "android" ? "http://10.11.150.101:5000" : "http://10.11.150.101:5000");
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
